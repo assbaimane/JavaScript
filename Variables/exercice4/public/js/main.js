14844bdcb98e218e906c76383adfa332c4605c51
Imane <assbaimane@hotmail.com>
2021-12-07 12:16:36 +0100

    Exercise 4 finished

---
+++ Variables/exercice4/public/js/main.js
@@ -0,0 +1,24 @@
+// Exo 4.1
+// var prenom = prompt("Quel est ton prénom?", "Prénom")
+// alert("Bonjour"+" "+prenom);
+
+// Exo 4.2
+// var year = prompt("How old are you?");
+// alert("You have "+year+" years old");
+
+// Exo 4.3
+// var year = prompt("How old are you?");
+// alert("You have "+year+" years old");
+
+// Exo 4.4
+// var surname = prompt("Whats your surname?");
+// var nom = prompt("Whats your name");
+// var fullName = surname + " " + nom
+// alert("Your full Name is " + fullName);
+
+// Exo 4.5
+var surname = prompt("Whats your surname?");
+var nom = prompt("Whats your name");
+var nom = nom.toUpperCase();
+var fullName = nom + " " + surname;
+alert("Your full Name is " + fullName);
