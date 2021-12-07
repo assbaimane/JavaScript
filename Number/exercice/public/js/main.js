d6dbc18f658ee1d2ad41c21138e94361a1825574
Imane <assbaimane@hotmail.com>
2021-12-07 19:53:26 +0100

    All number exercises finished

---
+++ Number/exercice/public/js/main.js
@@ -0,0 +1,48 @@
+// Exo 1, 2
+let nombre = 8.56;
+nombreProche = Math.round(nombre);
+console.log("Entier arrondi le plus proche : " + nombreProche);
+
+// Exo 3
+let nombreHaut = Math.ceil(nombre);
+console.log("Entier arrondi vers le haut : " + nombreHaut);
+
+// Exo 4
+let nombreBas = Math.floor(nombre);
+console.log("Entier arrondi vers le bas : " + nombreBas);
+
+// Exo 5
+let nombreRetire = Math.trunc(nombre);
+console.log("Partie décimale retirée : " + nombreRetire);
+
+// Exo 6
+let nombreMultiple = Math.random()
+console.log("Chiffre entier au hasard compris entre 0 et 1 : " + nombreMultiple);
+
+// Exo7
+let nombre100 = nombreMultiple * 100;
+console.log("Nombre au hasard compris entre 0 et 100 : " + nombre100);
+
+// Exo 8
+let nombre100Int = Math.round(nombre100);
+console.log("Nombre entier au hasard compris entre 0 et 100 : " + nombre100Int);
+
+// Exo 9
+let puissance2 = Math.pow(8,2);
+console.log("8 puissance 2 donne : " + puissance2);
+
+// Exo 10
+let racine9 = Math.sqrt(9);
+console.log("Racine cubique de 9 est égale à : " + racine9);
+
+// Exo 11
+let absolue1 = Math.abs(-1);
+console.log("La valeur absolue de -1 est égale à : " + absolue1);
+
+// Exo 12
+let plusgrand = Math.max(-2,1000,8,57);
+console.log("Le plus grand nombre de la liste est le suivant : " + plusgrand);
+
+// Exo 13
+let pluspetit = Math.min(-2, 1000, 8, 57);
+console.log("Le plus petit nombre de la liste est le suivant : " + pluspetit);
