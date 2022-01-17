986e1fd20d82c0eeddb59bad280095d26462d79a
Imane <assbaimane@hotmail.com>
2022-01-17 09:28:08 +0100

    Initialisation

--- DOM/Exercices/dom_ecouteur_evenement_part5_exercice2_les_events_avec_parametre/main.js
+++ DOM/Exercices/dom_ecouteur_evenement_part5_exercice2_les_events_avec_parametre/main.js
@@ -1,2 +1,20 @@
 // Pour l'exercice 2
 let pContent = "Il est possible de passer des parametres dans la function utilisé dans l'écouteur d'événement !"
+// > Event Target
+// ### 1. Créer un programme qui au clique d'un des éléments de la div l'élément s'affiche dans la console, en utilisant une boucle pour appliquer un écouteur d'événement sur chaque élément !
+let div = document.querySelector("#content");
+div.addEventListener("click",(e)=>{
+    console.log(e.EventTarget)
+});
+
+// ### 2. Créer un programme qui au double click d'un élément son texte est en majuscule
+
+// ### 3. Créer un programme qui au hasard choisis parmis les propriétés suivants et l'applique aux éléments quand on clique sur le button GO : 
+// - border: 1px solid gold;
+// - background-color: blue;
+// - background-color: red;
+// - border: 5px dotted gold;
+let go = document.querySelector("button");
+function hasard(){
+
+}
