15d56fd8a23b9ebc7a67c3b4763794bb00435051
Imane <assbaimane@hotmail.com>
2022-01-18 09:23:02 +0100

    Exercises 3-4-5 finished

--- cs-miniprojet-recapjs/public/js/main.js
+++ cs-miniprojet-recapjs/public/js/main.js
@@ -1,8 +1,10 @@
+
 // EXERCICE 1 ______________ JAVASCRIPT 
 let firstH2 = document.querySelector("h2");
 firstH2.innerText = "Le titre modifié";
 firstH2.style.color = "blue"
 firstH2.classList.add("bg-light");
+
 
 // EXERCICE 2 ______________ JAVASCRIPT 
 let navBar = document.querySelector("nav");
@@ -56,6 +58,43 @@ navBar.addEventListener("click",(event)=>{
     }else{}
 })
 
+
 // EXERCICE 3 ______________ JAVASCRIPT 
 let firstInput = document.querySelector("input");
-firstInput.value
+let firstSpan = document.querySelector("span");
+let goButton = document.querySelector(".go")
+goButton.addEventListener("click",()=>{
+    firstSpan.textContent = firstInput.value;
+    firstInput.value = "";
+})
+
+
+// EXERCICE 4 ______________ JAVASCRIPT 
+let changeButton = document.querySelector("#changePlace");
+let nbClick = document.querySelectorAll("span")[1];
+let boxMobile = document.querySelector(".rouge");
+let box1 = document.querySelector("#source")
+let box2 = document.querySelector("#destination");
+let counter = 0;
+changeButton.addEventListener("click",()=>{
+    if (boxMobile.parentElement == box1){
+        box2.appendChild(boxMobile);
+    }
+    else if(boxMobile.parentElement == box2){
+        box1.appendChild(boxMobile);
+    }else{}
+    counter++;
+    nbClick.innerText = counter;
+});
+
+
+// EXERCICE 5 ______________ JAVASCRIPT 
+let addEgg = document.querySelector(".add")
+addEgg.addEventListener("click",()=>{
+    let newEgg = document.createElement("img");
+    newEgg.src = "./public/img/oeuf.png";
+    newEgg.style.width = "20px";
+    addEgg.parentElement.appendChild(newEgg);
+})
+
+// EXERCICE 6 ______________ JAVASCRIPT 
